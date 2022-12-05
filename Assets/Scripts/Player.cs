e31e4cffba61f6cbf3876e10ac42e76ec041c26a
João Vitor Morais <joao.morais00@hotmail.com>
2022-12-05 13:43:47 -0300

    OnGround Fixed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -26,42 +26,54 @@ public class Player : MonoBehaviour
         
         anim = GetComponent<Animator>();
         groundCheck = gameObject.transform.Find("GroundCheck");
+        // Debug.Log(groundCheck.position);
         currentSpeed = maxSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(transform.position);
+        Debug.Log(groundCheck.position);
+
         onGround = Physics.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
         // Debug.Log(onGround);
-        if(Input.GetButtonDown("Jump") && !onGround){
+        if(Input.GetButtonDown("Jump") && onGround){
             // Debug.Log("teste");
             jump = true;
         }
     }
 
     private void FixedUpdate(){
+        
         if(!isDead){
+            
             float h = Input.GetAxis("Horizontal");
             float z = Input.GetAxis("Vertical");
-            
+            // Debug.Log(onGround);
+            // Debug.Log(h);
+            // Debug.Log(z);
             if(!onGround){
+            // Debug.Log("sempre entra");
+
                 z=0;
-
-                rb.velocity = new Vector3(h*currentSpeed, rb.velocity.y, z * currentSpeed);
-
-                if(h>0 && !facingRight){
-                    Flip();
-                }else if(h<0 && facingRight){
-                    Flip();
-                }
-                Debug.Log("pulou");
-                if(jump){
-                    Debug.Log("pulou");
-                    jump = false;
-                    rb.AddForce(Vector3.up * jumpForce);
-                }
             }
+
+            rb.velocity = new Vector3 (h*currentSpeed, rb.velocity.y, z*currentSpeed);
+            // Debug.Log(rb.velocity);
+
+
+            if(h>0 && !facingRight){
+                Flip();
+            }else if(h<0 && facingRight){
+                Flip();
+            }
+            if(jump){
+                Debug.Log("pulou");
+                jump = false;
+                rb.AddForce(Vector3.up * jumpForce);
+            }
+            
         }
     }
 
